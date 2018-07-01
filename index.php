e6aa83f6667ef45532c1bd427b1562741909cca8
Elelion <Elelion@yandex.ru>
2018-07-01 12:29:24 +0400

    landing page

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<HTML>
+
+<HEAD> 
+	<!-- SEO -->
+	<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+	<meta name="Keywords" content="Fullstack Development, Fullstack, Development">
+	<link rel = "shortcut icon" type = "image/x-icon" href = "images/favicon.png"> 
+	<title> Quest-1, maket-1 </title>
+
+	<!-- CSS's -->  
+	<link rel="stylesheet/less" href="css/styles.css">        	  
+	<script src="lib/less.min.js"></script>
+</HEAD>
+
+<BODY>
+<header>
+	<div class="header__logo">
+		<svg width="465px" height="465px">
+			<circle r="178" cx="180" cy="38.7%" 
+					fill="none" stroke="#056cb4" stroke-width="2"></circle>
+
+			<circle r="156" cx="180" cy="38.7%" 
+					fill="#056cb4" ></circle>
+			
+			<!--
+			<polygon fill="red" points="78,36 
+										36,106 
+										126,60 
+										25,60 
+										116,106" />
+			-->
+			<!-- 48,16                16,96         96,48       0,48           80,96" -->
+			<!-- center-top , left-bottom , right-top , left-top , right-bottom-->
+		</svg>			
+	</div>
+
+	<div class="header__titles">
+		<div class="header__titles--text1">
+			<p class="title_h1">name here</p>
+		</div>
+
+		<div class="header__titles--text2">
+			<p class="title_h2">surname</p>
+		</div>
+
+		<div class="header__titles--text3">
+			<p class="title_h3">graphic designer</p>
+		</div>
+	</div>
+
+	
+	</div>
+</header>
+
+<!-- .... -->
+
+<main>
+	<!-- ... -->
+</main>		
+
+<!-- .... -->
+
+<footer>
+	<!-- ... -->
+</footer>
+
+</BODY>
+</HTML>
