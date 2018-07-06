e5208589c3856bdb38ed3a791f5615701529b912
Elelion <Elelion@yandex.ru>
2018-07-07 00:23:58 +0400

    creating EDUCATION block

--- index.php
+++ index.php
@@ -101,7 +101,81 @@
 <!-- .... -->
 
 <main>
-	<!-- ... -->
+	<div class="main__education">
+		<div class="chapter">
+			<div class="chapter__logo">
+				<svg width="120px" height="120px">
+					<circle r="56" cx="58" cy="50.7%" 
+							fill="none" stroke="#056cb4" stroke-width="2"></circle>
+
+					<circle r="50" cx="58" cy="50.7%" fill="#056cb4" ></circle>
+					
+					<g transform="translate(20,98) scale(0.100000,-0.100000)"
+						fill="white" stroke="none" x="12" y="-10">
+
+					<path d="M329 573 l-44 -128 -144 -3 -143 -3 108 -75 c60 -42 112 -79 117 -83
+							4 -4 -13 -66 -37 -137 -24 -71 -43 -130 -42 -131 1 -1 52 34 114 77 
+							62 44 116 80 120 80 4 -1 57 -36 117 -79 61 -43 111 -78 112 -77 2 2 
+							-17 61 -41 132 l-43 130 113 80 113 79 -141 5 -141 5 -41 122 c-23 
+							67 -44 124 -47 128 -4 3 -26 -52 -50 -122z"/>
+					</g>
+				</svg>
+			</div>
+
+			<div class="chapter__title">
+				<p class="title__chapter">education</p>
+			</div>
+
+			<div class="chapter__education--line">
+				<svg width="500px" height="120px">
+					<line x1="12" y1="58" x2="494px" y2="58" 
+						  stroke="#056cb4" stroke-width="8"></line>
+				</svg>
+			</div>			
+		</div>
+
+		<div class="chapter__education--text">
+			<ul>
+				<?php 
+					addEducationBlock('master degree', 
+										'Feb 2011 - Jun 2014', 
+										'university name', 
+										'Lorem ipsum dolors sit amet, consectetuer adipiscing 
+										elit. Aenean commodo ligula eget dolor. Aenean massa. 
+										Cum sociis natoque penatibus et magnis dis parturient 
+										montes, nascetur ridiculus mus. Donec quam felis, 
+										ultricies nec, pellentesque.');
+
+					addEducationBlock('master degree', 
+										'Feb 2011 - Jun 2014', 
+										'university name', 
+										'Lorem ipsum dolors sit amet, consectetuer adipiscing 
+										elit. Aenean commodo ligula eget dolor. Aenean massa. 
+										Cum sociis natoque penatibus et magnis dis parturient 
+										montes, nascetur ridiculus mus. Donec quam felis, 
+										ultricies nec, pellentesque.');
+
+					addEducationBlock('master degree', 
+										'Feb 2011 - Jun 2014', 
+										'university name', 
+										'Lorem ipsum dolors sit amet, consectetuer adipiscing 
+										elit. Aenean commodo ligula eget dolor. Aenean massa. 
+										Cum sociis natoque penatibus et magnis dis parturient 
+										montes, nascetur ridiculus mus. Donec quam felis, 
+										ultricies nec, pellentesque.');
+
+					addEducationBlock('master degree', 
+										'Feb 2011 - Jun 2014', 
+										'university name', 
+										'Lorem ipsum dolors sit amet, consectetuer adipiscing 
+										elit. Aenean commodo ligula eget dolor. Aenean massa. 
+										Cum sociis natoque penatibus et magnis dis parturient 
+										montes, nascetur ridiculus mus. Donec quam felis, 
+										ultricies nec, pellentesque.');
+				?>						
+			</ul>
+		</div>
+	</div>
 </main>		
 
 <!-- .... -->
