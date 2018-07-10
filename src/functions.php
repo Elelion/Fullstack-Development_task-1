d57480b43efdc7f9b37b1a4ee568bd9be60d831a
Elelion <Elelion@yandex.ru>
2018-07-10 22:14:22 +0400

    add additional DIV for SKILLS block (for adaptive)

--- src/functions.php
+++ src/functions.php
@@ -5,23 +5,25 @@ function addSkillsBlock($name, $point) {
 		<li>
 			<p class="description__chapter"><?= $name; ?></p>
 
-			<!-- active skills points -->				
-			<?php for ($i = 0; $i < $point; $i++) { ?>
-				<div class="skills__point">
-					<svg width="30px" height="30px">
-						<circle r="13.5" cx="15" cy="15" fill="#056cb4" ></circle>
-					</svg>
-				</div>
-			<?php }  ?>
-			
-			<!-- empty skills points -->
-			<?php for ($j = 0; $j < 12 - $point; $j++) { ?>
-				<div class="skills__point--template">
-					<svg width="30px" height="30px">
-						<circle r="13.5" cx="15" cy="15" fill="#E8E8E8" ></circle>
-					</svg>
-				</div>
-			<?php }  ?>
+			<!-- active skills points -->
+			<div class="points__bar">
+				<?php for ($i = 0; $i < $point; $i++) { ?>
+					<div class="skills__point">
+						<svg width="30px" height="30px">
+							<circle r="13.5" cx="15" cy="15" fill="#056cb4" ></circle>
+						</svg>
+					</div>
+				<?php }  ?>
+				
+				<!-- empty skills points -->
+				<?php for ($j = 0; $j < 12 - $point; $j++) { ?>
+					<div class="skills__point--template">
+						<svg width="30px" height="30px">
+							<circle r="13.5" cx="15" cy="15" fill="#E8E8E8" ></circle>
+						</svg>
+					</div>
+				<?php }  ?>
+			</div>
 		</li>
 	<?php
 }
